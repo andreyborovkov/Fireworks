01b59fb030ff73fceb002ceefa60baedf32b57ef
andreyborovkov <andrey_borovkov@yahoo.com>
2018-12-17 14:45:44 -0500

    options added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 
-
+  import './main.css';
   import Fireworks from 'fireworks-canvas';
  
   export default () => {
@@ -7,3 +7,15 @@
     const fireworks = new Fireworks(container);
     fireworks.start();
   };
+
+  const options = {
+    maxRockets: 50,            // max # of rockets to spawn
+    rocketSpawnInterval: 50, // millisends to check if new rockets should spawn
+    numParticles: 50,        // number of particles to spawn when rocket explodes (+0-10)
+    explosionMinHeight: 0.2,  // percentage. min height at which rockets can explode
+    explosionMaxHeight: 0.9,  // percentage. max height before a particle is exploded
+    explosionChance: 0.08     // chance in each tick the rocket will explode
+  }
+
+  const fireworks = new Fireworks(container, options)
+  const stop = fireworks.start()
