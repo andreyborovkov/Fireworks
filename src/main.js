cb38d3ead29bb2846b86af88cdd0bcd9e71b9caa
andreyborovkov <andrey_borovkov@yahoo.com>
2018-12-17 14:10:38 -0500

    MVP

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
-export default () => {
-    alert('it works!');
+
+
+  import Fireworks from 'fireworks-canvas';
+ 
+  export default () => {
+    const container = document.getElementById('container');
+    const fireworks = new Fireworks(container);
+    fireworks.start();
   };
